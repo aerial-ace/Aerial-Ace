84d10cdbcd85a0b08f1f542f502314aa508604cf
DevGa_me <devm19102003@gmail.com>
2022-01-28 22:19:36 +0530

    Mail info now pops up randomly

--- cogs/mail.py
+++ cogs/mail.py
@@ -1,12 +1,10 @@
 import discord
 from discord.ext import commands
+import random
 
 import config
 
 class MailModule(commands.Cog):
-
-    state : bool = False
-
     @commands.guild_only()
     @commands.command(name="mail", aliases=["ml"])
     async def open_mail(self, ctx):
@@ -42,37 +40,17 @@ class MailModule(commands.Cog):
 
         await ctx.send(embed=embd)
 
-    @commands.is_owner()
-    @commands.guild_only()
-    @commands.command(name="mail_state")
-    async def set_mail_state(self, ctx, state):
-        if state.lower() == "on":
-            self.state = True
-        elif state.lower() == "off":
-            self.state = False
-        else:
-            raise commands.errors.BadArgument()
-
-    @set_mail_state.error
-    async def set_mail_state_handler(self, ctx, error):
-        if isinstance(error, commands.errors.BadArgument):
-            await ctx.reply("Gib On/Off as a parameter too :/")
-        elif isinstance(error, commands.errors.NotOwner):
-            await ctx.reply("Be owner of this bot when :>")
-        else:
-            await ctx.reply(error)
-
-mail_module = None
-
 # Mail reminder
 async def process_mail(ctx):
 
-        embd = discord.Embed(title="Mail Box", color=config.NORMAL_COLOR)
-        embd.description = ":email: Check the new mails using `-aa mail`"
-        await ctx.send(embed=embd)
+        prob : int = 30
+        roll = random.randint(0, 100)
+
+        if roll > 0 and roll < prob:
+            embd = discord.Embed(title="Mail Box", color=config.NORMAL_COLOR)
+            embd.description = ":email: Check the new mails using `-aa mail`"
+            await ctx.send(embed=embd)
 
 def setup(bot):
-    global mail_module
-
     mail_module = MailModule()
     bot.add_cog(mail_module)
