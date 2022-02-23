749efe6e3dd286771260dab73e27332589aea7b0
DevGa_me <devm19102003@gmail.com>
2022-02-23 20:44:18 +0530

    Mail for rerun of starboard command

--- cogs/mail.py
+++ cogs/mail.py
@@ -14,21 +14,20 @@ class MailModule(commands.Cog):
         embd = discord.Embed(title="__Mail Box - Aerial Ace__", color=config.NORMAL_COLOR)
         embd.description = f"{config.IMPORTANT_EMOJI} For Admins {config.IMPORTANT_EMOJI}\n\n"
 
-        embd.description += f"Aerial Ace now has a Starboard System. Use `{ctx.prefix}starboard #channel` to enable it.\n\n"
-        embd.description += f"Here is how the embeds look {config.THEFK_EMOJI}"
-        embd.set_image(url="https://cdn.discordapp.com/attachments/908392246158700607/944602162900635658/unknown.png")
+        embd.description += f"Random Team Commands are here :tada:\nCheck using `-aa help random_misc`\n\n"
+        embd.description += f"{config.ALERT_EMOJI} Admins are requested to rerun the starboard command if they are using it {config.THEFK_EMOJI}"
 
         await ctx.send(embed=embd)
 
 # Mail reminder
 async def process_mail(ctx):
 
-        prob : int = 20
+        prob : int = 100
         roll = random.randint(0, 100)
 
         if roll > 0 and roll < prob:
             embd = discord.Embed(title=f"{config.ALERT_EMOJI} Mail Box {config.ALERT_EMOJI}", color=config.NORMAL_COLOR)
-            embd.description = f"Starboard System is here :tada:\n\n"
+            embd.description = f"{config.ALERT_EMOJI}Admins are requested to rerun the starboard command\n\n"
             embd.description += f"Check using `{ctx.prefix}mail` now"
             await ctx.send(embed=embd)
 
