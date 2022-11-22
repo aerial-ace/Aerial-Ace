294ca30555a142d225cbd32209fcca3fe9d8b8f4
VikaDev <devm19102003@gmail.com>
2022-11-22 16:57:49 +0530

    FIX : Reduced the mail reminder probablity

--- cogs/mail.py
+++ cogs/mail.py
@@ -41,7 +41,7 @@ class MailModule(commands.Cog):
 # Mail reminder
 async def process_mail(ctx):
 
-        prob : int = 75
+        prob : int = 15
         roll = random.randint(0, 100)
 
         if roll > 0 and roll < prob:
