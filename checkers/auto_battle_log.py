14f06cc92fae6d3407e2a2040d0d400c76e06e28
Dev <devm1910@gmail.com>
2023-01-29 01:58:33 +0530

    ADD : Working Auto Logs.

--- checkers/auto_battle_log.py
+++ checkers/auto_battle_log.py
@@ -32,8 +32,6 @@ async def determine_battle_message(bot:AutoShardedBot, message:Message):
         
     def get_battle_message_from_poketwo(_message:Message):
 
-        global target_id
-
         if _message.author.id != int(config.POKETWO_ID):
             return False
         
@@ -42,18 +40,17 @@ async def determine_battle_message(bot:AutoShardedBot, message:Message):
         for keyword in battle_invitation_keywords:
             if keyword not in _message.content:
                 return False
-
-        target_id = int(_message.content.split()[1].removeprefix("<@").removesuffix(">"))
+            
+        if _message.mentions[0].id != target_id:
+            return False
 
         return True
     
     try:
-
         invitation_message = await bot.wait_for("message", check=get_battle_message_from_poketwo, timeout=10)
 
     except TimeoutError as t:
-
-        return await message.channel.send("Auto Logging Session Ended!")
+        return
     
     def get_confirmation_on_battle_invitation(reaction:Reaction, user:Member):
 
@@ -69,14 +66,21 @@ async def determine_battle_message(bot:AutoShardedBot, message:Message):
         return True
         
     try:
-
-        await bot.wait_for("reaction_add", check=get_confirmation_on_battle_invitation, timeout=30)
+        await bot.wait_for("reaction_add", check=get_confirmation_on_battle_invitation, timeout=10)
 
     except TimeoutError as t:
+        return await message.channel.send("Invitation was not accepted! Session Ended!")
 
-        return message.channel.send("Invitation was not accepted! Session Ended!")
+    else:
+        await message.channel.send("Auto Battle Log Session Started!")
+
+    conclusion_type = None
+    winner          = None
+    loser           = None
 
     def get_battle_cancel_message(msg:Message):
+
+        nonlocal conclusion_type
 
         if msg.author.id != challenger_id and msg.author.id != target_id:
             return False
@@ -88,24 +92,34 @@ async def determine_battle_message(bot:AutoShardedBot, message:Message):
         
         for keyword in battle_cancel_keywords:
             if keyword not in msg.content:
-                return False 
+                return False
 
-        print("Battle Cancelled by {}".format(msg.author.id))
+        conclusion_type = "CANCEL"
+
         return True
     
     def get_battle_end_message(msg:Message):
 
-        if msg.author.id != int(config.ADMIN_ID):
-            return False 
+        nonlocal conclusion_type, winner, loser
+
+        if msg.author.id != int(config.POKETWO_ID):
+            return False
+    
+        if "won the battle!" in msg.content:
+            winner = int(msg.content.split()[0].removeprefix("<@").removesuffix(">"))
+        else:
+            if "has won." in msg.content:
+                winner = int(msg.content.removesuffix("> has won.").split()[-1].removeprefix("<@"))
+            else:
+                return False
         
-        if "won the battle!" not in msg.content:
+        if winner != challenger_id and winner != target_id:
             return False
         
-        winner = int(msg.content.split()[0].removeprefix("<@").removesuffix(">"))
+        conclusion_type = "FINISH"
+        
         loser  = (target_id if winner == challenger_id else challenger_id)
 
-        #await message.channel.send("Winner : {}\nLoser : {}".format(winner, loser))
-        print("Winner : {}\nLoser : {}".format(winner, loser))
         return True
 
     def get_battle_conclusion(msg:Message):
@@ -119,8 +133,19 @@ async def determine_battle_message(bot:AutoShardedBot, message:Message):
         return False
 
     try:
-        await bot.wait_for("message", check=get_battle_conclusion, timeout=20)
-    except TimeoutError as t:
+        await bot.wait_for("message", check=get_battle_conclusion, timeout=10*60)
+
+    except TimeoutError:
         await message.channel.send("Auto Battle Logging session ended with a timeout.")
+
+    if conclusion_type == "CANCEL":
+        return await message.channel.send("Auto Log Session Cancelled!")
     
-    print("Session Ended!")
+    elif conclusion_type == "FINISH":
+        await message.channel.send("> Logging Battle. Please Wait!")
+
+        reply = await battle_helper.register_battle_log(message.guild.id, str(winner), str(loser))
+
+        await message.channel.send(reply)
+
+    print("Session Ended Successfully!")
