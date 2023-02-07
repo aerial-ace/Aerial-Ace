c253a204ae5049c2850cbc04f0ffcd0f706ac430
Dev <devm19102003@gmail.com>
2023-02-07 15:28:40 +0530

    ADD : New Mail for galar stats

--- cogs/mail.py
+++ cogs/mail.py
@@ -16,10 +16,10 @@ class MailModule(commands.Cog):
         view = DonationView(2000)
         
         embd.description = ">>>>>>>__**New**__\n\n"
-        embd.description += f"All servers will now receive access to Auto Battle Logging Module. This module ( if turned on ) will automatically log battle results to the server battle board. \nUse `-aa abl` to toggle this module. \nUse `-aa blb` to check this server's battle board. \nUse `-aa help battle` for help in battle related commands."
+        embd.description += f"After **A VERY LONG** time, we finally have Galar Pokemon Moves in Poketwo. We are working towards including all the required battle data for these galar pokemon, so that you can build your best teams and hopefully survive the new meta that is going to develop around these galar pokemons. \nWe have already included **Galar Stats**, and you can check those using `-aa stats <pokemon>` command. \n\n**NOTE** : Galar Pokemons are a very new introduction to the previous battle system, so it will take time until we all figure out which stats are best for which pokemon. So take these stats with a grain of salt, We do encourage players to try out different sets and help us make our stats database robust and error proof. \n\nThanks for using Aerial Ace as always <3"
 
         embd.description += "\n\n>>>>>>>__**Old**__\n\n"
-        embd.description += f"All premium servers will now receive Streak Detection System to log your 100th, 1000th, 10000th, 100000th, 1000000th, 10...(etc) catches to starboard. \nComes with all new look and feel. \nDonate to support the development and get this feature ( with many others ) right now\n\n"
+        embd.description += f"All servers will now receive access to Auto Battle Logging Module. This module ( if turned on ) will automatically log battle results to the server battle board. \nUse `-aa abl` to toggle this module. \nUse `-aa blb` to check this server's battle board. \nUse `-aa help battle` for help in battle related commands."
 
         await ctx.send(embed=embd, view=view)
 
@@ -31,7 +31,7 @@ async def process_mail(ctx):
 
         if roll > 0 and roll < prob:
             embd = discord.Embed(title=f"{config.ALERT_EMOJI} NOTICE {config.ALERT_EMOJI}", color=config.NORMAL_COLOR)
-            embd.description = f"Introducing **Automatic Battle Logging!** Out now in all servers."
+            embd.description = f"Introducing **Galar Stats** data in aerial ace."
             try:
                 embd.set_footer(text=f"Check the complete mail using {ctx.prefix}mail")
             except:
