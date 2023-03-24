00c47addb23900cb6cc72694a28245293030c1c4
Devanshu Mishra <devm19102003@gmail.com>
2023-03-24 18:31:40 +0530

    ADD : Mail Information for Random Rulesets.

--- cogs/mail.py
+++ cogs/mail.py
@@ -16,10 +16,10 @@ class MailModule(commands.Cog):
         view = DonationView(2000)
         
         embd.description = ">>>>>>>__**New**__\n\n"
-        embd.description += f"Due to the recent database refreshing, some servers might face problem with logging battle or with any other battle command. If so please report your server id at the support server."
+        embd.description += f"Introducing the all-new Random Ruleset feature of Aerial Ace. Are you bored of doing your usual battles? Are in for some fun battle experience? Run the command `-aa random_ruleset` to fetch random rules for your next battle. Enforce these rules on your battles to make them fun and challenging. There are many rules added, and you can contribute your own rulesets too. Hope on our Support Server and suggest your own cool ruleset for everyone to try. Good luck on your next rando battles."
 
         embd.description += "\n\n>>>>>>>__**Old**__\n\n"
-        embd.description += f"Galar Battle Data is now available in Aerial Ace. Train your pokemons now."
+        embd.description += f"Due to the recent database refreshing, some servers might face problem with logging battle or with any other battle command. If so please report your server id at the support server."
 
         embd.description += "\n\nThanks for using Aerial Ace as always."
 
@@ -28,12 +28,12 @@ class MailModule(commands.Cog):
 # Mail reminder
 async def process_mail(ctx):
 
-        prob : int = 10
+        prob : int = 30
         roll = random.randint(0, 100)
 
         if roll > 0 and roll < prob:
             embd = discord.Embed(title=f"{config.ALERT_EMOJI} NOTICE {config.ALERT_EMOJI}", color=config.NORMAL_COLOR)
-            embd.description = f"**Battle Data is refreshed** \nIf Battle Leaderboard Commands are not working in your server, please report the issue at the support server."
+            embd.description = f"**Random Rulesets** are here! Try them now!"
             try:
                 embd.set_footer(text=f"Check the complete mail using {ctx.prefix}mail")
             except:
