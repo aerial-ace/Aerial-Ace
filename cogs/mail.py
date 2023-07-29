abba63b7901c53b0ee636090d724bdd87dc8081b
Devanshu Mishra <devm19102003@gmail.com>
2023-07-29 17:57:43 +0530

    ADD : Mail for SV movesets.

--- cogs/mail.py
+++ cogs/mail.py
@@ -16,10 +16,10 @@ class MailModule(commands.Cog):
         view = DonationView(2000)
         
         embd.description = ">>>>>>>__**New**__\n\n"
-        embd.description += f"Introducing the all-new Random Ruleset feature of Aerial Ace. Are you bored of doing your usual battles? Are in for some fun battle experience? Run the command `-aa random_ruleset` to fetch random rules for your next battle. Enforce these rules on your battles to make them fun and challenging. There are many rules added, and you can contribute your own rulesets too. Hope on our Support Server and suggest your own cool ruleset for everyone to try. Good luck on your next rando battles."
+        embd.description += f"All the Scarlet/Violet Pokemon Movesets are now added! "
 
         embd.description += "\n\n>>>>>>>__**Old**__\n\n"
-        embd.description += f"Due to the recent database refreshing, some servers might face problem with logging battle or with any other battle command. If so please report your server id at the support server."
+        embd.description += f"Introducing the all-new Random Ruleset feature of Aerial Ace. Are you bored of doing your usual battles? Are in for some fun battle experience? Run the command `-aa random_ruleset` to fetch random rules for your next battle. Enforce these rules on your battles to make them fun and challenging. There are many rules added, and you can contribute your own rulesets too. Hope on our Support Server and suggest your own cool ruleset for everyone to try. Good luck on your next rando battles."
 
         embd.description += "\n\nThanks for using Aerial Ace as always."
 
@@ -29,7 +29,7 @@ class MailModule(commands.Cog):
 async def process_mail(ctx):
 
         embd = discord.Embed(title=f"{config.ALERT_EMOJI} NOTICE {config.ALERT_EMOJI}", color=config.NORMAL_COLOR)
-        embd.description = f"**Random Rulesets** are here! Try them now!"
+        embd.description = f"**Scarlet/Violet** pokemon movesets are here!"
         try:
             embd.set_footer(text=f"Check the complete mail using {ctx.prefix}mail")
         except:
