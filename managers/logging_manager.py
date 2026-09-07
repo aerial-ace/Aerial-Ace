06abb7b75c3d30ac9360c9b1b94b7100ec9f1483
staticaron <static.aron@gmail.com>
2026-09-07 21:59:44 +0000

    FIX: Not using default logger anymore

--- managers/logging_manager.py
+++ managers/logging_manager.py
@@ -1,18 +1,22 @@
-import logging, os
+import logging
+import os
 from logging.handlers import TimedRotatingFileHandler
+
+main_logger = logging.getLogger("main")
+
 
 def setup_logging():
 
     if not os.path.exists("logs/"):
         os.mkdir("logs/")
 
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-    
+    main_logger.setLevel(logging.INFO)
+
     file_handler = TimedRotatingFileHandler("logs/aerial-ace.log", when="midnight", interval=1, backupCount=7)
 
     formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-    formatter.datefmt = '%Y-%m-%d %H:%M:%S'
+    formatter.datefmt = "%Y-%m-%d %H:%M:%S"
     file_handler.setFormatter(formatter)
 
-    logger.addHandler(file_handler)
+    main_logger.addHandler(file_handler)
+
